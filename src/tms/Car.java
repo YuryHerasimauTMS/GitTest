27078cc000a24a618d18ead9ec82f8ec627db53c
YuryHerasimauTMS <sessor-wow@yandex.ru>
2021-07-14 21:22:42 +0300

    first commit

---
+++ src/tms/Car.java
@@ -0,0 +1,17 @@
+package tms;
+
+public abstract class Car {
+
+    private final int WHEELS_COUNT = 4;
+
+    public Car(){
+    }
+
+    public void drive(){
+        System.out.println("Машина едет");
+    }
+
+    public void sound(){
+        System.out.println("Машина делает бип-бип");
+    }
+}
