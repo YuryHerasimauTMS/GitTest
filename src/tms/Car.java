f65bba6b4e59c3384b9a497c71b1901d272580d9
YuryHerasimauTMS <sessor-wow@yandex.ru>
2021-07-14 21:43:53 +0300

    UPD: modify class Car

--- src/tms/Car.java
+++ src/tms/Car.java
@@ -3,12 +3,13 @@ package tms;
 public abstract class Car {
 
     private final int WHEELS_COUNT = 4;
+    private final String ENGINE = "Engine";
 
     public Car(){
     }
 
     public void drive(){
-        System.out.println("Машина едет");
+        System.out.println("Машина едет быстро");
     }
 
     public void sound(){
